1c9f352121e885533b8c76f6b69600e250eef385
Reza Soltani <rsoltani97@gmail.com>
2019-07-04 17:57:59 +0430

    Add fragment container

--- app/src/main/java/proj1/tamamtamam/thewall/MainActivity.java
+++ app/src/main/java/proj1/tamamtamam/thewall/MainActivity.java
@@ -3,13 +3,11 @@ package proj1.tamamtamam.thewall;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,16 +16,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_category:
-                    mTextMessage.setText(R.string.title_category);
                     return true;
                 case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_search);
                     return true;
                 case R.id.navigation_submit:
-                    mTextMessage.setText(R.string.title_submit);
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.fragment_container, new PostFragment());
+                    ft.commit();
                     return true;
             }
             return false;
@@ -39,9 +36,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_container, new PostFragment());
+        ft.commit();
     }
 
 }
