203e45795793a8c736c155f996031d0191a8ef30
hoda <hodat9696@gmail.com>
2019-07-04 18:15:01 +0430

    post recycler view added

--- app/src/main/java/proj1/tamamtamam/thewall/MainActivity.java
+++ app/src/main/java/proj1/tamamtamam/thewall/MainActivity.java
@@ -4,12 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mTextMessage;
+
+    private RecyclerView mRecyclerView;
+    private PostsRecyclerViewAdapter adapter;
+    private List<Post> postList;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -42,6 +53,20 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        postList = new ArrayList<>();
+        postList.add(new Post(1, "a used laptop", "downtown resort", "yesterday"));
+        postList.add(new Post(2, "a used charger", "downtown resort", "yesterday"));
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL));
+        adapter = new PostsRecyclerViewAdapter(postList, this);
+        mRecyclerView.setAdapter(adapter);
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
